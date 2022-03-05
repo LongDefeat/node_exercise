7046de4be5bfa2e0cf3be5306c423d4c650ead33
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2022-03-05 15:37:54 -0800

    init upload

---
+++ node_exercise/node-files/step3.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const process = require("process");
+const axios = require("axios");
+/** read file at path and print it out. */
+
+function cat(path) {
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+async function webCat(url) {
+  try {
+    let res = await axios.get(url);
+    console.log(res.data);
+  } catch (err) {
+    console.error(`Error fetching ${url}: ${err}`);
+    process.exit(1);
+  }
+}
+
+let path;
+let out;
+
+if (process.argv[2] === "--out") {
+  out = process.argv[3];
+  path = process.argv[4];
+} else {
+  path = process.argv[2];
+}
+
+if (path.slice(0, 4) === "http") {
+  webCat(path, out);
+} else {
+  cat(path, out);
+}
